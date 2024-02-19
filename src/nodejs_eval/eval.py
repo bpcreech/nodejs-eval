ce08fd36bb22ceea0359abf7d39430991902337b
Ben Creech <hi@bpcreech.com>
2024-02-19 16:12:31 -0500

    Use async and aiohttp instead of sync requests

--- src/nodejs_eval/eval.py
+++ src/nodejs_eval/eval.py
@@ -1,38 +1,53 @@
-from contextlib import contextmanager
+from aiohttp import ClientSession, UnixConnector
+from asyncio import sleep
+from contextlib import asynccontextmanager
 from nodejs.npx import Popen
 from os import getpgid, killpg
 from os.path import exists, join
-from requests_unixsocket import Session
 from signal import SIGINT
 from tempfile import TemporaryDirectory
-from polling2 import poll
+from time import time
 
-@contextmanager
-def Evaluator():
+
+async def _poll(checker, step, timeout):
+	start = time()
+	while True:
+		if checker():
+			return
+
+		if time() - start > timeout:
+			raise RuntimeError('Timed out')
+
+		await sleep(step)
+
+@asynccontextmanager
+async def Evaluator():
 	"""Create a JavaScript evaluator.
 
-	This function returns a context manager. To use:
+	This function returns an async context manager. To use:
 
-	    with Evaluator() as e:
-	    	e.eval('return 6*7;')
+	    async with Evaluator() as e:
+	    	await e.eval('return 6*7;')
 	"""
 
 	tmpDir = TemporaryDirectory('nodejs-eval')
 	try:
 		sockName = join(tmpDir.name, 'http.sock')
-		escaped = sockName.replace('/', '%2F')
-		sockUrl = f'http+unix://{escaped}/run'
 
 		p = Popen(
 			['http-eval', 'http-eval', '--udsPath', sockName],
 			start_new_session=True)
 		try:
 			# Wait for the server to open the socket:
-			poll(lambda: exists(sockName), timeout=5, step=0.1)
+			await _poll(lambda: exists(sockName), timeout=5, step=0.1)
 
-			with Session() as session:
-				yield _Evaluator(session, sockUrl)
+			async with UnixConnector(path=sockName) as conn:
+				async with ClientSession(connector=conn) as session:
+					yield _Evaluator(session)
 		finally:
+			# npx creates subprocesses. If we just interrupt our child,
+			# subprocesses will remain behind. Instead we interrupt the whole
+			# process group:
 			pgrp = getpgid(p.pid)
 			killpg(pgrp, SIGINT)
 			p.wait()
@@ -40,18 +55,17 @@ def Evaluator():
 		tmpDir.cleanup()
 
 class _Evaluator:
-	def __init__(self, session, sockUrl):
+	def __init__(self, session):
 		self.__session = session
-		self.__sockUrl = sockUrl
 
-	def eval(self, cmd, isAsync=False):
-		res = self.__session.post(
-			self.__sockUrl,
-			json={'code': cmd},
-			params={'async': str(isAsync).lower()})
-		j = res.json()
+	async def eval(self, cmd, isAsync=False):
+		async with self.__session.post(
+				'http://bogus/run',
+				json={'code': cmd},
+				params={'async': str(isAsync).lower()}) as res:
+			j = await res.json()
 
-		if 'error' in j:
-			raise RuntimeError(f"Error evaluating JavaScript: {j['error']}")
+			if 'error' in j:
+				raise RuntimeError(f"Error evaluating JavaScript: {j['error']}")
 
-		return j['result']
+			return j['result']
