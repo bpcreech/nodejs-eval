c575447784396bef3bf7cccc102a24cf364a3cde
Ben Creech <hi@bpcreech.com>
2024-02-19 17:22:23 -0500

    fmt

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,50 +1,58 @@
 from asyncio import gather
-import pytest
-from nodejs_eval.eval import evaluator, JavaScriptError
 from time import time
+
+import pytest
+
+from nodejs_eval.eval import JavaScriptError, evaluator
 
 
 @pytest.mark.asyncio
 async def test_basic_sync():
     async with evaluator() as e:
-        result = await e.run('return 6*7;')
+        result = await e.run("return 6*7;")
         assert result == 42
+
 
 @pytest.mark.asyncio
 async def test_exception_sync():
     async with evaluator() as e:
         with pytest.raises(JavaScriptError) as exc_info:
-            await e.run('return foo;')
+            await e.run("return foo;")
 
-        assert 'ReferenceError: foo is not defined' in exc_info.value.args[0]
+        assert "ReferenceError: foo is not defined" in exc_info.value.args[0]
+
 
 @pytest.mark.asyncio
 async def test_state_sync():
     async with evaluator() as e:
-        await e.run('this.x = 6*7;')
-        result = await e.run('return this.x;')
+        await e.run("this.x = 6*7;")
+        result = await e.run("return this.x;")
         assert result == 42
+
 
 @pytest.mark.asyncio
 async def test_basic_async():
     async with evaluator() as e:
-        result = await e.run_async('return 6*7;')
+        result = await e.run_async("return 6*7;")
         assert result == 42
+
 
 @pytest.mark.asyncio
 async def test_exception_async():
     async with evaluator() as e:
         with pytest.raises(JavaScriptError) as exc_info:
-            await e.run_async('return foo;')
+            await e.run_async("return foo;")
 
-        assert 'ReferenceError: foo is not defined' in exc_info.value.args[0]
+        assert "ReferenceError: foo is not defined" in exc_info.value.args[0]
+
 
 @pytest.mark.asyncio
 async def test_state_async():
     async with evaluator() as e:
-        await e.run_async('this.x = 6*7;')
-        result = await e.run_async('return this.x;')
+        await e.run_async("this.x = 6*7;")
+        result = await e.run_async("return this.x;")
         assert result == 42
+
 
 @pytest.mark.asyncio
 async def test_import_async():
@@ -52,15 +60,18 @@ async def test_import_async():
         result = await e.run_async('let os = await import("os"); return os.cpus();')
         assert len(result) > 0
 
+
 @pytest.mark.asyncio
 async def test_concurrency_async():
     async with evaluator() as e:
         start = time()
         futures = [
-            e.run_async('''
+            e.run_async(
+                """
 await new Promise(r => setTimeout(r, 2000));
 return 42;
-''')
+"""
+            )
             for _ in range(10)
         ]
         results = await gather(*futures)
